e0f2f6b337a0f42a5bfba999ea088be6a098c64f
lordwarmax <max870123@hotmail.com>
2020-06-01 19:58:26 -0500

    Adding firebase

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyB_ITYP3o2k2mgsSbmr0K6Dsa19Vor-BOg",
+    authDomain: "crown-db-6ae55.firebaseapp.com",
+    databaseURL: "https://crown-db-6ae55.firebaseio.com",
+    projectId: "crown-db-6ae55",
+    storageBucket: "crown-db-6ae55.appspot.com",
+    messagingSenderId: "5674677256",
+    appId: "1:5674677256:web:1a28f7d2772897995b8dbd",
+    measurementId: "G-78JV5YHHMS"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
